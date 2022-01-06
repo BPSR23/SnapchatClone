8c0790394edc24e6c8662f54da450abf37449cb0
ramprakashreddy <36103674+ramprakashreddy@users.noreply.github.com>
2022-01-06 17:06:41 +0530

    Firebase Storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,24 @@
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 //import "firebase/compat/firestore";
-import 'firebase/firestore';
+import "firebase/firestore";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getStorage, ref } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYSFvq7PUX5gUWmVf9gL1IyIiR8lYvMC8",
-    authDomain: "snapchat-clone-b.firebaseapp.com",
-    projectId: "snapchat-clone-b",
-    storageBucket: "snapchat-clone-b.appspot.com",
-    messagingSenderId: "721043526964",
-    appId: "1:721043526964:web:003d1f12b3dbef34538161"
-  };
+  apiKey: "AIzaSyDYSFvq7PUX5gUWmVf9gL1IyIiR8lYvMC8",
+  authDomain: "snapchat-clone-b.firebaseapp.com",
+  projectId: "snapchat-clone-b",
+  storageBucket: "snapchat-clone-b.appspot.com",
+  messagingSenderId: "721043526964",
+  appId: "1:721043526964:web:003d1f12b3dbef34538161",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
-  const storage = firebase.storage();
-  const provider = new firebase.auth.GoogleAuthProvider();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = firebase.auth();
+const storeRef = getStorage(firebaseApp);
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = ref(storeRef);
 
-  export { db, auth, storage, provider };
-  
+export { db, auth, storage, provider };
